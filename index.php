8b4bfbe9000f2ea0078eb9795c1d5f2c8d5bf238
David Cummings <fayecummings@fayes-mbp.austin.rr.com>
2014-05-25 21:49:39 -0500

    scaffoding

--- index.php
+++ index.php
@@ -4,28 +4,25 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Template</title>
+    <title>New Project</title>
 
-    <!-- Bootstrap -->
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/styles.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
   <body>
-
-  
-
-    <h1>Hello, world!</h1>
-
-
-
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    
+    <section class="container">
+      <div class="content row">
+      <?php include "_/components/php/header.php"; ?> 
+      <?php include "_/components/php/footer.php"; ?>   
+      </div> <!-- content -->
+    </section> <!-- container -->
+    
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/script.js"></script>
   </body>
